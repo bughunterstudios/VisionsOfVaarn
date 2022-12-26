21e6b08584128d54685ed779e22ad55974fe1a79
bughunterstudios <bughunterstudios@gmail.com>
2022-12-26 15:58:20 -0700

    General fixes. Start of dialogue system.

---
+++ Assets/Scripts/DialogueHolder.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueHolder : MonoBehaviour
+{
+    public GameObject promptholder;
+    public GameObject optionsholder;
+
+    public GameObject text_prefab;
+    public GameObject button_prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
