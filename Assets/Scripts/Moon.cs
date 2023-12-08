afa1952f5dfdcd80c1a879209f78cabd863fe3a6
Jacob Marks <bughunterstudios@gmail.com>
2023-12-08 10:56:53 -0700

    Many changes to skybox.

---
+++ Assets/Scripts/Moon.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moon : MonoBehaviour
+{
+    public float MoonCycleInMinutes;
+
+    public Transform sun;
+
+    private float rotations;
+    private float y, z;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        y = transform.rotation.eulerAngles.y;
+        z = transform.rotation.eulerAngles.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotations += Time.deltaTime * (360f / (MoonCycleInMinutes * 60f));
+        transform.Rotate(Time.deltaTime * (360f / (MoonCycleInMinutes * 60f)), 0, 0, Space.Self);
+
+        Shader.SetGlobalVector("_MoonAngles", new Vector3(rotations, y, z));
+
+        float dotproduct = (Vector3.Dot(transform.forward, sun.forward) + 1f) / 2f;
+        Debug.Log(dotproduct);
+        Shader.SetGlobalFloat("_Eclipse", dotproduct);
+    }
+}
--- Assets/Scripts/SunScript.cs
+++ Assets/Scripts/SunScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SunScript : MonoBehaviour
 {
     public Gradient fog;
+    public Gradient clouds;
 
     public float DayLengthInMinutes;
     public Gradient light_level;
@@ -21,18 +22,18 @@ public class SunScript : MonoBehaviour
         max_intensity = GetComponent<Light>().intensity;
     }
 
-        // Update is called once per frame
+    // Update is called once per frame
     void Update()
     {
         rotations += Time.deltaTime * (360f / (DayLengthInMinutes * 60f));
-        transform.rotation = Quaternion.Euler(rotations, y, z);
+        transform.Rotate(Time.deltaTime * (360f / (DayLengthInMinutes * 60f)), 0, 0, Space.Self);
 
         Shader.SetGlobalVector("_SunDirection", transform.forward);
         Shader.SetGlobalVector("_SunAngles", new Vector3(rotations, y, z));
-        Shader.SetGlobalFloat("_SkyRotation", rotations);
 
         float dotproduct = (Vector3.Dot(transform.forward, Vector3.down) + 1f) / 2f;
         RenderSettings.fogColor = fog.Evaluate(dotproduct);
+        Shader.SetGlobalColor("_CloudColor", clouds.Evaluate(dotproduct));
 
         GetComponent<Light>().intensity = light_level.Evaluate(dotproduct).r * max_intensity;
     }
